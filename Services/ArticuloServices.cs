92849037d3aa64b370faa7a062fbec741126f7f6
estban03 <estebanmutuverria003@gmail.com>
2024-10-18 23:53:10 -0300

    add ArticuloServices, MarcaServices and CategoriaServices

---
+++ Services/ArticuloServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+using Models;
+
+namespace Services
+{
+    internal class ArticuloServices
+    {
+    }
+}
