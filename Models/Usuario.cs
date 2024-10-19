22eb7bc4016ddf91be6a4aea08d64819595ca498
AlejoLg <legname.alejo@gmail.com>
2024-10-19 19:54:17 -0300

    Usuario model

---
+++ Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contrasena { get; set; }
+        public string Rol { get; set; }
+    }
+
+}
