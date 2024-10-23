3cc5810102ae1ff3932d874c62abb78bc16f0f6b
Carlosss8 <carlosrodriguez8ra@gmail.com>
2024-10-23 01:39:33 -0300

    New class "Compra" and "Venta".

---
+++ Models/Venta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Models
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+
+        public int IdCliente { get; set; }
+
+        public DateTime FechaVenta { get; set; }
+
+        public string NumeroFactura { get; set; }
+    }
+}
