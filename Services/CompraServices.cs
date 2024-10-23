c9913f8308b20272231043f7300a9016088a879f
Carlosss8 <carlosrodriguez8ra@gmail.com>
2024-10-23 01:41:58 -0300

    New Class "CompraServices" and VentaServices

---
+++ Services/CompraServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+using Models;
+
+namespace Services
+{
+    public class CompraServices
+    {
+        private DataBaseAccess DB = new DataBaseAccess();
+
+        public void IngresarCompra(int IdProveedor, DateTime FechaCompra)
+        {
+            try
+            {
+                dbAccess.setQuery("INSERT INTO vw_InsertarCompra (IdProveedor, FechaCompra) VALUES (@IdProveedor, @FechaCompra)");
+
+                dbAccess.setParameter("@IdProveedor", IdProveedor);
+                dbAccess.setParameter("@FechaCompra", FechaCompra);
+
+                dbAccess.excecuteAction();
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            finally
+            {
+               DB.CloseConnection();
+            }
+        }
+    }
+}
