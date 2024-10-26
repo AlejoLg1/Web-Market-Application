f646e8ae8427d7afe2fbda3922cba445e3f61e60
Carlosss8 <carlosrodriguez8ra@gmail.com>
2024-10-26 17:04:50 -0300

    Changes  "DetalleCompraServices", "DetalleVentaServices" new list

---
+++ Services/DetalleCompraServices.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using Utils;
+
+namespace Services
+{
+   public class DetalleCompraService
+    {
+        private DataBaseAccess DB = new DataBaseAccess();
+
+        public List<DetalleCompra> listar()
+        {
+            List<DetalleCompra> list = new List<DetalleCompra>();
+            try
+            {
+                DB.setQuery("Select IdDetalleCompra, IdCompra, IdProducto, Cantidad, PrecioUnitario from DetalleCompra");
+                DB.excecuteQuery();
+
+                while (DB.Reader.Read())
+                {
+                    DetalleCompra detalleCompra = new DetalleCompra();
+                    detalleCompra.IdDetalleCompra = (int)DB.Reader["IdDetalleCompra"];
+                    detalleCompra.IdCompra = (int)DB.Reader["IdCompra"];
+                    detalleCompra.IdProducto = (int)DB.Reader["IdProducto"];
+                    detalleCompra.Cantidad = (int)DB.Reader["Cantidad"];
+                    detalleCompra.PrecioUnitario = (decimal)DB.Reader["PrecioUnitario"];
+
+                    list.Add(detalleCompra);
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DB.CloseConnection();
+            }
+        }
+    }
+}
---
+++ Services/DetalleVentaServices.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using Utils;
+
+namespace Services
+{
+   public class DetalleVentaServices
+    {
+        private DataBaseAccess DB = new DataBaseAccess();
+
+        public List<DetalleVenta> listar()
+        {
+            List<DetalleVenta> list = new List<DetalleVenta>();
+            try
+            {
+                DB.setQuery("Select IdDetalleVenta, IdVenta, IdProducto, Cantidad, PrecioUnitario, PrecioTotal from DetalleVenta");
+                DB.excecuteQuery();
+
+                while (DB.Reader.Read())
+                {
+                    DetalleVenta detalleVenta = new DetalleVenta();
+                    detalleVenta.IdDetalleVenta = (int)DB.Reader["IdDetalleVenta"];
+                    detalleVenta.IdVenta = (int)DB.Reader["IdVenta"];
+                    detalleVenta.IdProducto = (int)DB.Reader["IdProducto"];
+                    detalleVenta.Cantidad = (int)DB.Reader["Cantidad"];
+                    detalleVenta.PrecioUnitario = (decimal)DB.Reader["PrecioUnitario"];
+                    detalleVenta.PrecioTotal = (decimal)DB.Reader["PrecioTotal"];
+
+                    list.Add(detalleVenta);
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DB.CloseConnection();
+            }
+        }
+    }
+}
