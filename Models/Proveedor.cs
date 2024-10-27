612bf6cff7e58839ad191ea063c992b55b6aacb7
Carlosss8 <carlosrodriguez8ra@gmail.com>
2024-10-27 20:24:07 -0300

    New class "ProveedorProductoServices" + list and "ProveedorServices" + list

--- Models/Proveedor.cs
+++ Models/Proveedor.cs
@@ -11,6 +11,7 @@ namespace Models
         public int IdProveedor { get; set; }
         public string Nombre { get; set; }
         public string Correo { get; set; }
+        public string Telefono { get; set; }
         public string Direccion { get; set; }
 
     }
---
+++ Services/ProveedorServices.cs
@@ -0,0 +1,49 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace Services
+{
+    public class ProveedorServices
+    {
+        
+        private DataBaseAccess DB = new DataBaseAccess();
+
+        public List<Proveedor> listar()
+        {
+            List<Proveedor> list = new List<Proveedor>();
+            try
+            {
+                DB.setQuery("Select IdProveedor, Nombre, Correo, Telefono, Direccion from Proveedor");
+                DB.excecuteQuery();
+
+                while (DB.Reader.Read())
+                {
+                    Proveedor proveedor = new Proveedor();
+                    proveedor.IdProveedor = (int)DB.Reader["IdProveedor"];
+                    proveedor.Nombre = (string)DB.Reader["Nombre"];
+                    proveedor.Telefono = (string)DB.Reader["Correo"];
+                    proveedor.Correo = (string)DB.Reader["Telefono"];
+                    proveedor.Direccion = (string)DB.Reader["Direccion"];
+
+                    list.Add(proveedor);
+                }
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DB.CloseConnection();
+            }
+        }
+    }
+}
+
