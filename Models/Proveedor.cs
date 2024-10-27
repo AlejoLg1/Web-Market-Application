3e868a61ab4c9f36fa9bc106112f3432b5177df8
Carlosss8 <carlosrodriguez8ra@gmail.com>
2024-10-27 15:53:30 -0300

    New class "Proveedor", "ProveedorProducto"

---
+++ Models/Proveedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Proveedor
+    {
+        public int IdProveedor { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
---
+++ Models/ProveedorProducto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ProveedorProducto
+    {
+        public int IdProveedorProducto { get; set; }
+        public int IdProveedor { get; set; }
+        public int IdProducto { get; set; }
+        public decimal PrecioCompra { get; set; }
+        public DateTime FechaCompra { get; set; }
+    }
+}
