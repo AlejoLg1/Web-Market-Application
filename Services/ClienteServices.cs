c0f16fdae7d29ca4d214e8cf8aa5455c8b11d826
AlejoLg <legname.alejo@gmail.com>
2024-10-28 22:36:03 -0300

    ClienteServices ABM

--- Services/ClienteServices.cs
+++ Services/ClienteServices.cs
@@ -50,5 +50,76 @@ namespace Services
                 DB.CloseConnection();
             }
         }
+
+        public void add(Cliente newCliente)
+        {
+            try
+            {
+                DB.clearParameters();
+                DB.setQuery("INSERT INTO Cliente (Nombre, Apellido, Correo, Telefono, Direccion) VALUES (@Nom, @Apel, @Cor, @Tel, @Dir)");
+
+                DB.setParameter("@Nom", newCliente.Nombre);
+                DB.setParameter("@Apel", newCliente.Apellido);
+                DB.setParameter("@Cor", newCliente.Correo);
+                DB.setParameter("@Tel", newCliente.Telefono);
+                DB.setParameter("@Dir", newCliente.Direccion);
+
+                DB.excecuteAction();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"FATAL ERROR: Error al crear Cliente. Comuníquese con el Soporte.\nDetalles: {ex.Message}");
+            }
+            finally
+            {
+                DB.CloseConnection();
+            }
+        }
+
+        public void modify(Cliente cliente)
+        {
+            try
+            {
+                DB.clearParameters();
+                DB.setQuery("UPDATE Cliente SET Nombre = @Nom, Apellido = @Apel, Correo = @Cor, Telefono = @Tel, Direccion = @Dir WHERE IdCliente = @Id");
+
+                DB.setParameter("@Nom", cliente.Nombre);
+                DB.setParameter("@Apel", cliente.Apellido);
+                DB.setParameter("@Cor", cliente.Correo);
+                DB.setParameter("@Tel", cliente.Telefono);
+                DB.setParameter("@Dir", cliente.Direccion);
+                DB.setParameter("@Id", cliente.IdCliente);
+
+                DB.excecuteAction();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"FATAL ERROR: Error al modificar Cliente. Comuníquese con el Soporte.\nDetalles: {ex.Message}");
+            }
+            finally
+            {
+                DB.CloseConnection();
+            }
+        }
+
+        public void delete(int id)
+        {
+            try
+            {
+                DB.clearParameters();
+                DB.setQuery("DELETE FROM Cliente WHERE IdCliente = @Id");
+                DB.setParameter("@Id", id);
+                DB.excecuteAction();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"FATAL ERROR: Error al eliminar Cliente. Comuníquese con el Soporte.\nDetalles: {ex.Message}");
+            }
+            finally
+            {
+                DB.CloseConnection();
+            }
+        }
+
     }
 }
