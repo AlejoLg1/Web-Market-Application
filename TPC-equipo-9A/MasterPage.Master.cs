917aaf51134b20c0560be7b54c511032fe83ccc1
AlejoLg <legname.alejo@gmail.com>
2024-10-30 21:09:37 -0300

    Login and Rol validations to access pages

--- TPC-equipo-9A/MasterPage.Master.cs
+++ TPC-equipo-9A/MasterPage.Master.cs
@@ -1,5 +1,6 @@
 ﻿using Models;
 using Services;
+using Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace TPC_equipo_9A
 {
     public partial class MasterPage : System.Web.UI.MasterPage
     {
+
         UsuarioServices service = new UsuarioServices();
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -18,9 +20,22 @@ namespace TPC_equipo_9A
             {
                 if (!IsPostBack)
                 {
+
                     if (Session["rol"] != null)
                     {
                         string userRole = Session["rol"].ToString();
+                        int userId = Convert.ToInt32(Session["id"]);
+                        bool isEditing = Request.QueryString["edit"] != null;
+                        bool isInProfile = Request.QueryString["id"] != null;
+
+                        if (Page is AgregarUsuario || (Page is Perfil && (isEditing || isInProfile)))
+                        {
+                            if (!Utils.Security.HaveAccess(userRole))
+                            {
+                                Response.Redirect("Default.aspx", false);
+                            }
+                        }
+                        
 
                         if (!string.IsNullOrEmpty(userRole))
                         {
@@ -43,7 +58,7 @@ namespace TPC_equipo_9A
                             }
 
                             imgUser.ImageUrl = "/images/user.png";
-                            Usuario user = service.getUser(Convert.ToInt32(Session["id"]));
+                            Usuario user = service.getUser(userId);
                             if (user != null)
                             {
                                 imgUser.ImageUrl = user.FotoPerfil.ToString();
@@ -56,6 +71,14 @@ namespace TPC_equipo_9A
                             Response.Redirect("Error.aspx", false);
                         }
                     }
+                    else
+                    {
+                        if(!(Page is Login))
+                        {
+                            Response.Redirect("Login.aspx", false);
+                            return;
+                        }
+                    }
                 }
             }
             catch (Exception ex)
@@ -65,7 +88,7 @@ namespace TPC_equipo_9A
             }
         }
 
-        protected void btnLogout_Click(object sender, EventArgs e)
+           protected void btnLogout_Click(object sender, EventArgs e)
         {
             Session.Abandon();
             Response.Redirect("Login.aspx");
