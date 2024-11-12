65221910fce7e62ce23d17b47eb113c2f5678ef4
Carlosss8 <carlosrodriguez8ra@gmail.com>
2024-11-11 22:04:43 -0300

    New attribute

--- Models/Compra.cs
+++ Models/Compra.cs
@@ -9,10 +9,11 @@ namespace Models
     public class Compra
     {
         public int IdCompra { get; set; }
-
         public int IdProveedor { get; set; }
-
         public DateTime FechaCompra { get; set; }
         public bool Estado {  get; set; }
+        public Proveedor Proveedor { get; set; }
+        public DetalleCompra DetalleCompra { get; set; }
+
     }
 }
