bc4074d279f3976edfa0e2c7c6230d0b20cb3015
Carlosss8 <carlosrodriguez8ra@gmail.com>
2024-11-18 19:44:26 -0300

    Rename the "IdVenta"

--- Models/Venta.cs
+++ Models/Venta.cs
@@ -9,7 +9,7 @@ namespace Models
 {
     public class Venta
     {
-        public int Id { get; set; }
+        public int IdVenta { get; set; }
         public int IdCliente { get; set; }
         public DateTime FechaVenta { get; set; }
         public string NumeroFactura { get; set; }
